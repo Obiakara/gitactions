4e3ba00cb60327c164b362ee23573f81ca9511de
Chinedu Obiakara <obiakarachinedu@gmail.com>
2022-01-03 20:46:24 -0600

    Edited yml file

--- common/shared_scripts/shared_scripts.sh
+++ common/shared_scripts/shared_scripts.sh
@@ -8,9 +8,9 @@ echo "-------------- Running unit tests before deployment ------------------"
 #coverage run --omit 'env/**/*' -m pytest -rp && coverage report -m
 #coverage run --omit 'env/**/*' -m pytest -rp
 coverage json --fail-under=95
+ls
 #x=$(coverage report --fail-under=70)
 coverage_file=coverage.json
-ls
 coverage_percentage=$(jq .totals.percent_covered_display $coverage_file)
 echo "$coverage_percentage"
 if [ $coverage_file == 70 ]; then
