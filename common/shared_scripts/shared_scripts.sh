a4a10e829d7411a336474410fcc34a76e8aeca41
Chinedu Obiakara <obiakarachinedu@gmail.com>
2022-01-03 20:49:31 -0600

    Edited yml file

--- common/shared_scripts/shared_scripts.sh
+++ common/shared_scripts/shared_scripts.sh
@@ -7,6 +7,7 @@ python3 -m venv env && source env/bin/activate && pip install -r requirements.tx
 echo "-------------- Running unit tests before deployment ------------------"
 #coverage run --omit 'env/**/*' -m pytest -rp && coverage report -m
 #coverage run --omit 'env/**/*' -m pytest -rp
+coverage run --omit 'env/**/*' -m
 coverage json --fail-under=95
 ls
 #x=$(coverage report --fail-under=70)
@@ -14,7 +15,7 @@ coverage_file=coverage.json
 coverage_percentage=$(jq .totals.percent_covered_display $coverage_file)
 echo "$coverage_percentage"
 if [ $coverage_file == 70 ]; then
-    coverage run --omit 'env/**/*' -m pytest -rp
+    pytest -rp
 else
     echo "Exiting"
     exit 1
