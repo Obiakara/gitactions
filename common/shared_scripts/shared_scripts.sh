33e15c238ff31fd2dd0af2fdd036b041c3038ac0
Chinedu Obiakara <obiakarachinedu@gmail.com>
2022-01-04 07:26:48 -0600

    Edited yml file

--- common/shared_scripts/shared_scripts.sh
+++ common/shared_scripts/shared_scripts.sh
@@ -9,13 +9,12 @@ coverage run --omit 'env/**/*' -m pytest -rp && coverage report -m
 coverage json --fail-under=50
 coverage_file=coverage.json
 
-coverage_mark="95"
 coverage_percentage=$(jq .totals.percent_covered_display $coverage_file)
 
-if [ "$coverage_percentage" < "$coverage_mark" ]; then
+if [ "$coverage_percentage" < "95" ]; then
     echo "GOOD ###############################"
 else
-    echo "Unit test coverage is less than $coverage_mark. Failing build"
+    echo "Unit test coverage is less than 95. Failing build"
     exit 1
 fi
 deactivate
