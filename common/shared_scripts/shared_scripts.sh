0674d95456aaef6b15e562a5b96f1e17910db6e3
Chinedu Obiakara <obiakarachinedu@gmail.com>
2022-01-05 13:00:29 -0600

    Git ignore

--- common/shared_scripts/shared_scripts.sh
+++ common/shared_scripts/shared_scripts.sh
@@ -5,12 +5,13 @@ python3 -m pip install --user virtualenv
 # shellcheck disable=SC1091
 python3 -m venv env && source env/bin/activate && pip install -r requirements.txt
 echo "-------------- Running unit tests before deployment ------------------"
+
 coverage run --omit 'env/**/*' -m pytest -rp && coverage report -m
 coverage json --fail-under=95
-coverage_file=coverage.json
+coverage_file="$coverage\.json"
 coverage_mark=95
 coverage_percentage=$(jq -r .totals.percent_covered_display $coverage_file)
-echo "$coverage_percentage"
+
 if [ "$coverage_percentage" -ge "$coverage_mark" ]; then
     echo "Unit test passed with accepted coverage"
 else
